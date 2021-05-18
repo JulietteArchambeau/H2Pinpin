2eeb60610f87af1af1a2d7f0b9960ff2309f2d88
JulietteArchambeau <juli.archambeau@orange.fr>
2021-05-18 16:43:00 +0200

    first commit after SSH init

--- scripts/DataMunging/00_CleaningPhenotypeDataNew.R
+++ scripts/DataMunging/00_CleaningPhenotypeDataNew.R
@@ -127,7 +127,7 @@ data[,col.cm] <- data[,col.cm] * 10
 
 
 # remane columns
-data <- data %>% rename(AST_survdec11 = AST_survdic11,
+data <- data %>% dplyr::rename(AST_survdec11 = AST_survdic11,
                         AST_htdec11 = AST_htdic11_cm, 
                         AST_htnov12 = AST_htnov12_cm, 
                         AST_htmar14 = AST_htmar14_cm,
@@ -181,7 +181,7 @@ data[data$tree=="VER12_3","AST_htmar14"] <- data[data$tree=="VER12_3","AST_htmar
 
 geno <- read.delim("data/ClonapinData/ClonapinBlups523IndPiMASSJuly2019.txt")
 geno <- geno[,c("X","prov","Q1","Q2","Q3","Q4","Q5")]
-geno <- geno %>% rename(clon=X)
+geno <- geno %>% dplyr::rename(clon=X)
 
 # Checking NAs
 sapply(geno, function(x) sum(is.na(x)))
@@ -226,7 +226,9 @@ data$clon[data$clon=="CAD14"] <- "CAR14"
 
 # Email of Santi the 16/02/2021: he thinks MAD of Bordeaux and MAD1 of Portugal adn Asturias are the same
 data[data$clon=="MAD"|data$clon=="MAD1",] %>% tbl_df %>% print(n = Inf)
-data$tree[data$clon=="MAD"] <- paste0(stri_sub(g,1,3),1,stri_sub(g,4,-1))
+data$tree[data$clon=="MAD"] <- paste0(stri_sub(data$tree[data$clon=="MAD"],1,3),
+                                      1,
+                                      stri_sub(data$tree[data$clon=="MAD"],4,-1))
 data$clon[data$clon=="MAD"] <- "MAD1"
 
 
@@ -316,7 +318,7 @@ dataSave <- data
 #=================================================================================="
 
 # Three rows in which there is only NAs
-dataNA <- data %>% select(d13C,contains("BDX"),contains("POR"),contains("AST"))
+dataNA <- data %>% dplyr::select(d13C,contains("BDX"),contains("POR"),contains("AST"))
 which(rowSums(is.na(dataNA))==ncol(dataNA))
 dataNA <- dataNA[which(rowSums(is.na(dataNA))==ncol(dataNA)),]
 dataNA
@@ -336,7 +338,7 @@ sapply(data, function(x) sum(is.na(x)))
 # Import & clean provenance coordinates
 prov.coord <- read_csv("data/ClonapinData/coordinates_provenances.csv")
 prov.coord <- prov.coord[,c("CODE","ALTITUDE","LATITUDE","LONGITUDE")] %>%
-  rename(prov=CODE,altitude_prov=ALTITUDE,latitude_prov=LATITUDE, longitude_prov=LONGITUDE) %>%
+  dplyr::rename(prov=CODE,altitude_prov=ALTITUDE,latitude_prov=LATITUDE, longitude_prov=LONGITUDE) %>%
   slice(1:35) # remove the last line with only NAs (which remained from the original table)
 
 # Merge provenance coordinates & phenotypic data
@@ -440,7 +442,7 @@ AST_dec11 <-  data[!(is.na(data$AST_survdec11)&is.na(data$AST_htdec11)),]
 AST_dec11$survival <- AST_dec11$AST_survdec11
 AST_dec11$height <- AST_dec11$AST_htdec11
 AST_dec11$age <- 10
-AST_dec11 <- AST_dec11 %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+AST_dec11 <- AST_dec11 %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                   clones522, age, survival, height)
 
 ####### november 2012
@@ -448,7 +450,7 @@ AST_nov12 <-  data[!(is.na(data$AST_survnov12)&is.na(data$AST_htnov12)),]
 AST_nov12$survival <- AST_nov12$AST_survnov12
 AST_nov12$height <- AST_nov12$AST_htnov12
 AST_nov12$age <- 21
-AST_nov12 <- AST_nov12 %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+AST_nov12 <- AST_nov12 %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                   clones522, age, survival, height)
 
 ####### march 2014
@@ -456,7 +458,7 @@ AST_mar14 <-  data[!(is.na(data$AST_survmar14)&is.na(data$AST_htmar14)),]
 AST_mar14$survival <- AST_mar14$AST_survmar14
 AST_mar14$height <- AST_mar14$AST_htmar14
 AST_mar14$age <- 37
-AST_mar14 <- AST_mar14 %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+AST_mar14 <- AST_mar14 %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                   clones522, age, survival, height)
 
 
@@ -467,7 +469,7 @@ BDX_nov12 <-  data[!(is.na(data$BDX_surv12)),]
 BDX_nov12$survival <- BDX_nov12$BDX_surv12
 BDX_nov12$height <- NA
 BDX_nov12$age <- 13
-BDX_nov12 <- BDX_nov12 %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+BDX_nov12 <- BDX_nov12 %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                   clones522, age, survival, height)
 
 ####### November 2013: survival and height
@@ -475,7 +477,7 @@ BDX_nov13 <-  data[!(is.na(data$BDX_surv13)&is.na(data$BDX_htnov13)),]
 BDX_nov13$survival <- BDX_nov13$BDX_surv13
 BDX_nov13$height <- BDX_nov13$BDX_htnov13
 BDX_nov13$age <- 25
-BDX_nov13 <- BDX_nov13 %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+BDX_nov13 <- BDX_nov13 %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                   clones522, age, survival, height)
 
 
@@ -484,7 +486,7 @@ BDX_pheno13 <-  data[!(is.na(data$BDX_s32013)&is.na(data$BDX_dbb2013)),]
 BDX_pheno13$bb <- BDX_pheno13$BDX_s32013
 BDX_pheno13$dbb <- BDX_pheno13$BDX_dbb2013
 BDX_pheno13$age <- 15 # age in January 2013
-BDX_pheno13 <- BDX_pheno13 %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+BDX_pheno13 <- BDX_pheno13 %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                   clones522, age, bb, dbb)
 
 
@@ -493,7 +495,7 @@ BDX_nov14 <-  data[!(is.na(data$BDX_surv14)&is.na(data$BDX_htnov14)),]
 BDX_nov14$survival <- BDX_nov14$BDX_surv14
 BDX_nov14$height <- BDX_nov14$BDX_htnov14
 BDX_nov14$age <- 37
-BDX_nov14 <- BDX_nov14 %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+BDX_nov14 <- BDX_nov14 %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                   clones522, age, survival, height)
 
 ####### Phenology 2014: duration of bud burst and bud burst
@@ -501,7 +503,7 @@ BDX_pheno14 <-  data[!(is.na(data$BDX_s32014)&is.na(data$BDX_dbb2014)),]
 BDX_pheno14$bb <- BDX_pheno14$BDX_s32014
 BDX_pheno14$dbb <- BDX_pheno14$BDX_dbb2014
 BDX_pheno14$age <- 27 # age in January 2014
-BDX_pheno14 <- BDX_pheno14 %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+BDX_pheno14 <- BDX_pheno14 %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                     clones522, age, bb, dbb)
 
 ####### November 2015: survival and height
@@ -509,7 +511,7 @@ BDX_nov15 <-  data[!(is.na(data$BDX_surv15)&is.na(data$BDX_htnov15)),]
 BDX_nov15$survival <- BDX_nov15$BDX_surv15
 BDX_nov15$height <- BDX_nov15$BDX_htnov15
 BDX_nov15$age <- 49
-BDX_nov15 <- BDX_nov15 %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+BDX_nov15 <- BDX_nov15 %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                   clones522, age, survival, height)
 
 ####### Phenology 2015: duration of bud burst and bud burst
@@ -517,7 +519,7 @@ BDX_pheno15 <-  data[!(is.na(data$BDX_s32015)&is.na(data$BDX_dbb2015)),]
 BDX_pheno15$bb <- BDX_pheno15$BDX_s32015
 BDX_pheno15$dbb <- BDX_pheno15$BDX_dbb2015
 BDX_pheno15$age <- 29 # age in January 2015
-BDX_pheno15 <- BDX_pheno15 %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+BDX_pheno15 <- BDX_pheno15 %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                       clones522, age, bb, dbb)
 
 ####### Phenology 2017: duration of bud burst and bud burst
@@ -525,7 +527,7 @@ BDX_pheno17 <-  data[!(is.na(data$BDX_s32017)&is.na(data$BDX_dbb2017)),]
 BDX_pheno17$bb <- BDX_pheno17$BDX_s32017
 BDX_pheno17$dbb <- BDX_pheno17$BDX_dbb2017
 BDX_pheno17$age <- 53 # age in January 2017
-BDX_pheno17 <- BDX_pheno17 %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+BDX_pheno17 <- BDX_pheno17 %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                       clones522, age, bb, dbb)
 
 ####### November 2018: survival and height 
@@ -533,7 +535,7 @@ BDX_nov18 <-  data[!(is.na(data$BDX_surv18)&is.na(data$BDX_htnov18)),]
 BDX_nov18$survival <- BDX_nov18$BDX_surv18
 BDX_nov18$height <- BDX_nov18$BDX_htnov18
 BDX_nov18$age <- 85
-BDX_nov18 <- BDX_nov18 %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+BDX_nov18 <- BDX_nov18 %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                   clones522, age, survival, height)
 
 
@@ -563,7 +565,7 @@ POR_jan12 <-  data[!(is.na(data$POR_survjan12)&is.na(data$POR_htjan12)),]
 POR_jan12$survival <- POR_jan12$POR_survjan12
 POR_jan12$height <- POR_jan12$POR_htjan12
 POR_jan12$age <- 11
-POR_jan12 <- POR_jan12 %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+POR_jan12 <- POR_jan12 %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                   clones522, age, survival, height)
 
 ####### may 2012
@@ -571,7 +573,7 @@ POR_may12 <-  data[!(is.na(data$POR_survmay12)&is.na(data$POR_htmay12)),]
 POR_may12$survival <- POR_may12$POR_survmay12
 POR_may12$height <- POR_may12$POR_htmay12
 POR_may12$age <- 15
-POR_may12 <- POR_may12 %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+POR_may12 <- POR_may12 %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                   clones522, age, survival, height)
 
 ####### october 2012
@@ -579,7 +581,7 @@ POR_oct12 <-  data[!(is.na(data$POR_survoct12)&is.na(data$POR_htoct12)),]
 POR_oct12$survival <- POR_oct12$POR_survoct12
 POR_oct12$height <- POR_oct12$POR_htoct12
 POR_oct12$age <- 20
-POR_oct12 <- POR_oct12 %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+POR_oct12 <- POR_oct12 %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                   clones522, age, survival, height)
 
 ####### may 2013
@@ -587,21 +589,21 @@ POR_may13 <-  data[!(is.na(data$POR_survmay13)&is.na(data$POR_htmay13)),]
 POR_may13$survival <- POR_may13$POR_survmay13
 POR_may13$height <- POR_may13$POR_htmay13
 POR_may13$age <- 27
-POR_may13 <- POR_may13 %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+POR_may13 <- POR_may13 %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                   clones522, age, survival, height)
 
 
 ####### d13C
 POR_d13C <-  data[!(is.na(data$d13C)),]
 POR_d13C$age <- NA
-POR_d13C <- POR_d13C %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+POR_d13C <- POR_d13C %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                       clones522, age, d13C)
 
 ####### SLA
 POR_SLA <-  data[!(is.na(data$POR_SLA)),]
 POR_SLA$SLA <- POR_SLA$POR_SLA
 POR_SLA$age <- NA
-POR_SLA <- POR_SLA %>% select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
+POR_SLA <- POR_SLA %>% dplyr::select(region, site, metap, prov, clon_original, block_original, clon, tree, block,
                                 clones522, age, SLA)
 
 
@@ -620,8 +622,12 @@ total <- bind_rows(AST_dec11,AST_nov12,AST_mar14,
 table(total$age,total$site)
 
 ### A column to define a different IDs per obs (several osb per tree)
-total <- total %>% mutate(obs = tree, num=NA) %>% group_by(tree) %>%  mutate(num = 1:n()) %>% 
-  unite(obs, obs, num, sep="_") %>% select(1:5,7,10,6,9,8,obs,11:ncol(total))
+total <- total %>% 
+  mutate(obs = tree, num=NA) %>% 
+  group_by(tree) %>%  
+  mutate(num = 1:n()) %>% 
+  unite(obs, obs, num, sep="_") %>% 
+  dplyr::select(1:5,7,10,6,9,8,obs,11:ncol(total))
 length(unique(total$obs))
 
 ######################################"
@@ -646,7 +652,7 @@ total <- total[!(total$prov=="SID"),]
 # Import & clean provenance coordinates
 prov.coord <- read_csv("data/ClonapinData/coordinates_provenances.csv")
 prov.coord <- prov.coord[,c("CODE","ALTITUDE","LATITUDE","LONGITUDE")] %>%
-  rename(prov=CODE,altitude_prov=ALTITUDE,latitude_prov=LATITUDE, longitude_prov=LONGITUDE) %>%
+  dplyr::rename(prov=CODE,altitude_prov=ALTITUDE,latitude_prov=LATITUDE, longitude_prov=LONGITUDE) %>%
   slice(1:35) # remove the last line with only NAs (which remained from the original table)
 
 # Merge provenance coordinates & phenotypic data
